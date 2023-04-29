e65419d0fa21384b08e47333609a1a28608c38a2
JHeaon <j3heawon@gmail.com>
2023-04-29 14:52:23 +0900

    Fix "Modify path"

--- main/braiile.py
+++ main/braiile.py
@@ -14,7 +14,7 @@ def play(img):
     opener.addheaders = [
         ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
     urllib.request.install_opener(opener)
-    path = "main/chromedriver.exe"
+    path = "./main/chromedriver.exe"
     driver = webdriver.Chrome(path)
 
     driver.get("https://angelina-reader.ru/")
